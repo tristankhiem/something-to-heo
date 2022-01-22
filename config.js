5e0337dd7d54803c4d2a5d5538e1066758f4c4f9
tristankhiem <60792300+tristankhiem@users.noreply.github.com>
2022-01-23 01:47:33 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Hoem bít ngày dề đây ta?",
+    introTitle: 'Heo iu ❤️',
+    introDesc: `Nếu Heo cần một người luôn yêu thương cưng chiều Heo, cùng Heo đi tiếp những chặng đường dài sắp tới thì đó là ai?`,
+    btnIntro: 'Pun chớ ai -.-',
+    title: 'Hôm nay là ngày có một thiên thần bé nhỏ được chào đời',
+    desc: 'Heo giống như món quà mà Tạo hóa đã ban tặng cho Pun ó. Từ khi gặp được Heo, cuộc sống Pun trở nên tươi sáng hơn nè, Pun cố gắng hơn nè, yêu thương Heo hơn nè. Cho dù nhiều lúc Pun với Heo hong có cùng quan điểm, nhưng mà mình vẫn lắng nghe nhau và hiểu cho nhau, nên mới quấn nhau cũng được gần 3 năm ròi đó. :3 Pun muốn cảm ơn Heo vì đã xuất hiện trong cuộc đời Pun. Pun rất hạnh phúc và sẽ trân trọng điều đó. Pun muốn hỏi Heo là liệu Heo có iu Pun nhiều hơn nữa hoem?',
+    btnYes: 'Iu lắm lunn ❤️❤️❤️',
+    btnNo: 'Iu cqq 🙂',
+    question: 'Heo có điều dề muốn nói với Pun hoem?',
+    btnReply: 'Gửi cho Pun <3',
+    reply: 'Cóoo, Heo muốn nói là Heo iu Pun rất nhìu và sẽ luôn ngoan để được Pun cưng hơn ó!!!',
+    mess: 'Òemmmmm 🥰. Pun cũng vại.',
+    messDesc: 'Chúc Heo tuổi mới, cũng như là một năm mới gặp được nhiều thuận lợi nè, học được thêm nhiều kiến thức mà Heo mong muốn để chuẩn bị được đủ hành trang và bước tiếp vào con đường phát triển sự nghiệp của mình nha. Lát nữa Pun dắt đi chơi haa!',
+    btnAccept: 'Ằmm',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
